0b857f80070d6e23a586c121f065eacc5cbb079c
Xyraa Code Name <106752922+XyraaCode@users.noreply.github.com>
2022-12-04 19:26:04 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
         try:
-                __import__("RUN").cek_lisen_aktif()
+                __import__("RUN.so").cek_lisen_aktif()
         except Exception as e:
                 exit(str(e))
